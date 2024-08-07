cec0b328eaf4c6f2ca11be98e35c971c8b844688
Alexseyka07 <alex07progtime@gmail.com>
2024-08-07 21:49:01 +0300

    add_models

--- search/models.py
+++ search/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class mark(models.Model):
+    name = models.CharField(max_length=200)
+    producer_country_name = models.CharField(max_length=200)
+    is_visible = models.BooleanField(default=True)
+
+
+class model(models.Model):
+    name = models.CharField(max_length=200)
+    mark = models.ForeignKey(mark, on_delete=models.CASCADE)
+    is_visible = models.BooleanField(default=True)
+
+
+class path(models.Model):
+    name = models.CharField(max_length=200)
+    mark = models.ForeignKey(mark, on_delete=models.CASCADE)
+    model = models.ForeignKey(model, on_delete=models.CASCADE)
+    producer_country_name = models.CharField(max_length=200)
+    is_visible = models.BooleanField(default=True)
+    
