9e8eb23c5f5da17b0dd39360bd80868de77a6734
Alexseyka07 <alex07progtime@gmail.com>
2024-08-08 14:24:36 +0300

    upd_models

--- search/models.py
+++ search/models.py
@@ -18,7 +18,7 @@ class path(models.Model):
     mark = models.ForeignKey(mark, on_delete=models.CASCADE)
     model = models.ForeignKey(model, on_delete=models.CASCADE)
     producer_country_name = models.CharField(max_length=200)
-    price = models.IntegerField()
+    price = models.IntegerField(null=True, default=0)
     json_data = models.JSONField(null=True)
     is_visible = models.BooleanField(default=True)
     
